e98f282332dc0daab907e6f36d68bc3197a5f512
lazyduck037 <crazyduck037@gmail.com>
2020-02-29 18:47:46 +0700

    - Added proxy for app

--- TaskAgile/front-end/tests/e2e/specs/test.js
+++ TaskAgile/front-end/tests/e2e/specs/test.js
@@ -5,7 +5,7 @@ module.exports = {
   'login test': function browser(browser) {
     browser
       .url(process.env.VUE_DEV_SERVER_URL + 'login')
-      .waitForElementVisible('#app', 5000)
+      .waitForElementVisible('#app', 10000)
       .assert.containsText('h1', 'TaskAgile')
       .end()
   }
