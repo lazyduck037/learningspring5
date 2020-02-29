117d4ab846c8c011bf9ab1cc4252a5a781326511
lazyduck037 <crazyduck037@gmail.com>
2020-02-29 21:15:12 +0700

    - added unit test for register page.

---
+++ TaskAgile/front-end/src/views/RegisterPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="register-form">
+        <div class="logo-wrapper">
+          <img class="logo" src="/static/images/logo.png">
+          <div class="tagline">Open source task management tool</div>
+        </div>
+        <form>
+          <div class="form-group">
+            <label>Username</label>
+            <input type="text" class="form-control" id="username" value="">
+          </div>
+          <div class="form-group">
+            <label>Email address</label>
+            <input type="email" class="form-control" id="emailAddress" value="">
+          </div>
+          <div class="form-group">
+            <label>Password</label>
+            <input type="password" class="form-control" id="password" value="">
+          </div>
+          <button type="submit" class="btn btn-primary btn-block">Create account</button>
+        </form>
+
+      </div>
+    </div>
+    <footer class="footer">
+      <span class="copyright">...</span>
+      <ul class="footer-links list-inline float-right">...</ul>
+    </footer>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .container {max-width: 900px;}
+  .register-form {margin-top: 50px; max-width: 320px;}
+  .logo-wrapper {margin-bottom: 40px}
+  .footer {width: 100%; line-height: 40px; margin-top: 50px;}
+</style>
