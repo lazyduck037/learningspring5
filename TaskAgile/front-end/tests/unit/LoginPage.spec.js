f0170a789e2ed459061d8ff880001be6ff47d6bb
datto <dat.to@saritasa.com>
2020-03-04 18:07:08 +0700

    - Implement login page.

--- TaskAgile/front-end/tests/unit/LoginPage.spec.js
+++ TaskAgile/front-end/tests/unit/LoginPage.spec.js
@@ -1,7 +1,41 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import VueRouter from 'vue-router'
 import LoginPage from '@/views/LoginPage'
+import {mount} from "@vue/test-utils";
+import authenticationService from '@/services/authentication'
+
+const localVue = createLocalVue()
+localVue.use(Vuelidate)
+localVue.use(VueRouter)
+const router = new VueRouter()
+
+jest.mock('@/services/authentication')
 
 describe('LoginPage.vue', () => {
+  let wrapper
+  let fieldUsername
+  let fieldPassword
+  let buttonSubmit
+  let authenticateSpy
+
+  beforeEach(()=>{
+    wrapper = mount(LoginPage,{
+      localVue,
+      router
+    })
+    fieldUsername = wrapper.find('#username')
+    fieldPassword = wrapper.find('#password')
+    buttonSubmit = wrapper.find('form button[type="submit"]')
+
+    authenticateSpy = jest.spyOn(authenticationService, 'authenticate')
+
+  })
+
+  afterEach(()=>{
+
+  })
+
   it('should render correct contents', () => {
     const Constructor = Vue.extend(LoginPage)
     const vm = new Constructor().$mount()
